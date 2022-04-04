f9c679deb0b8e15e778a0c918414f8a2249f4347
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-04 19:12:30 +0530

    1 file modified

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Input from '../Inputs/Input';
 const Card = (props) => {
-  const [location, setLocation] = useState(null);
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState('Ahmedabad');
+  useEffect(() => {
+    const featchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e7fc42d81dcab94ec1e0c74f098b1566`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      console.log(resJson);
+    };
+    featchApi();
+  });
+
   const getLocationHandler = (e) => {
     e.preventDefault();
     console.log('this is:', this);
 
-    setLocation(e.target.location);
+    setCity(e.target.location);
   };
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card text-center" style={{ width: '18rem' }}>
       <Input onChange={getLocationHandler} />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={60}
-        height={60}
-        fill="currentColor"
-        className="bi bi-geo-alt-fill"
-        viewBox="0 0 16 16"
-        {...props}
-      >
-        <path d="M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 110-6 3 3 0 010 6z" />
-      </svg>
-      <div className="card-body">
-        <h5 className="card-title">{location}</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
+
+      <div className="card-body align-center">
+        <h5 className="card-title">
+          {' '}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={60}
+            height={60}
+            fill="currentColor"
+            className="bi bi-geo-alt-fill"
+            viewBox="0 0 16 16"
+            {...props}
+          >
+            <path d="M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 110-6 3 3 0 010 6z" />
+          </svg>
+          Current location
+          {location}
+        </h5>
+        <a href="#" className="btn btn-primary my-3">
+          5.25 Cel
         </a>
+        <p className="card-text">Min 5.25 Cel | Max: 6.35. Cel</p>
       </div>
     </div>
   );
