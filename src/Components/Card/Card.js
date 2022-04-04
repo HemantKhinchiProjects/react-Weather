a391e5ef0bdb687e4a51ec167618b64608179c76
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-04 19:28:50 +0530

    1 file modified

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -8,16 +8,16 @@ const Card = (props) => {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e7fc42d81dcab94ec1e0c74f098b1566`;
       const response = await fetch(url);
       const resJson = await response.json();
-      console.log(resJson);
+      setCity(resJson);
     };
     featchApi();
   });
 
   const getLocationHandler = (e) => {
     e.preventDefault();
-    console.log('this is:', this);
+    //console.log('this is:', this);
 
-    setCity(e.target.location);
+    //setCity(e.target.location);
   };
   return (
     <div className="card text-center" style={{ width: '18rem' }}>
@@ -36,8 +36,7 @@ const Card = (props) => {
           >
             <path d="M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 110-6 3 3 0 010 6z" />
           </svg>
-          Current {city}
-          {location}
+          Current {city.name}
         </h5>
         <a href="#" className="btn btn-primary my-3">
           5.25 Cel
