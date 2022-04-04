fcc23fdab51e9cf9b2654da2fa61e7a89001cf77
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-04 12:45:21 +0530

    1 file modified

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import Input from './Inputs/Input';
-const Card = (props) => (
-  <div className="card" style={{ width: '18rem' }}>
-    <Input />
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={60}
-      height={60}
-      fill="currentColor"
-      className="bi bi-geo-alt-fill"
-      viewBox="0 0 16 16"
-      {...props}
-    >
-      <path d="M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 110-6 3 3 0 010 6z" />
-    </svg>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
+import React, { useState } from 'react';
+import Input from '../Inputs/Input';
+const Card = (props) => {
+  const [location, setLocation] = useState(null);
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <Input onClick={getLocation} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={60}
+        height={60}
+        fill="currentColor"
+        className="bi bi-geo-alt-fill"
+        viewBox="0 0 16 16"
+        {...props}
+      >
+        <path d="M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 110-6 3 3 0 010 6z" />
+      </svg>
+      <div className="card-body">
+        <h5 className="card-title">{location}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Card;
